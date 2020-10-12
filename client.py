7f023b4dcb92faf60682da75a9c1cb19762bdcc3
dojiherbs <nateherbert@gmail.com>
2020-10-11 21:24:31 -0500

    initial server/client build

---
+++ client.py
@@ -0,0 +1,33 @@
+import socket
+
+
+HEADER = 64
+PORT = 5050
+#SERVER = '192.168.1.47' #this is the device the serve will run off of. ipconfig
+SERVER = socket.gethostbyname(socket.gethostname())
+ADDR = (SERVER, PORT)
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE = "!DISCONNECT"
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(ADDR)
+
+
+def send(msg):
+    message = msg.encode(FORMAT)
+    msg_length = len(message)
+    send_length = str(msg_length).encode(FORMAT)
+
+    #message must be 64 bits to be valid
+    send_length += b' ' * (HEADER - len(send_length))
+    client.send(send_length)
+    client.send(message)
+
+
+send("Hello world!")
+
+
+send(input())
+
+
+send(DISCONNECT_MESSAGE)
---
+++ server.py
@@ -0,0 +1,62 @@
+import socket
+import threading
+
+
+#threading lets you seperate code out so it's not stacked waiting for code to finish
+
+HEADER = 64
+PORT = 5050
+#SERVER = '192.168.1.47' #this is the device the serve will run off of. ipconfig
+SERVER = socket.gethostbyname(socket.gethostname())
+ADDR = (SERVER, PORT)
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE = "!DISCONNECT"
+
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#anything that hits this address, hits this socket
+server.bind(ADDR)
+
+
+def handle_client(conn, addr):
+    print(f"[NEW CONNECTION]: {addr} connected.")
+
+    connected = True
+    while connected:
+        msg_length = conn.recv(HEADER).decode(FORMAT)
+        
+        if msg_length:
+            msg_length = int(msg_length)
+            msg = conn.recv(msg_length).decode(FORMAT)
+
+
+
+            if msg == DISCONNECT_MESSAGE:
+                connected = False
+
+            print(f"[{addr}]: {msg}")
+
+            
+
+    conn.close()
+
+def start():
+    server.listen()
+    print(f"[LISTENING]: Server is listening on {SERVER}")
+    while True:
+        #when a new connection occurs, we'll parse "socket object", "information about the connection"
+        conn, addr = server.accept()
+        thread = threading.Thread(target=handle_client, args=(conn, addr))
+        thread.start()
+        print(f"[# of ACTIVE CONNECTIONS]: {threading.activeCount() - 1}")
+    pass
+
+
+
+
+
+print("[STARTING]: Server is starting")
+start()
+
+
+
