da0196dab967016e192da48df1743c055dacdcd3
dojiherbs <nateherbert@gmail.com>
2020-10-13 10:26:41 -0500

    messing with display

--- board.py
+++ board.py
@@ -34,27 +34,48 @@ def get_weather(city):
 
 
 root = tk.Tk()
+root.title("Avalon")
 
 canvas = tk.Canvas(root, height=HEIGHT, width=WIDTH)
-canvas.pack()
+#canvas.pack()
+canvas.grid(row=0, column=0)
+
 
 background_image = tk.PhotoImage(file='landscape.png')
+
 background_label = tk.Label(root, image=background_image)
-background_label.place(relwidth=1, relheight=1)
+#background_label = tk.Label(root)
+
+
+#background_label.place(relwidth=1, relheight=1)
+background_label.grid(row=0, column=0)
 
 frame = tk.Frame(root, bg='#80c1ff', bd=5)
-frame.place(relx=0.5, rely=0.1, relwidth=0.75, relheight=0.1, anchor='n')
+#frame.place(relx=0.5, rely=0.1, relwidth=0.75, relheight=0.1, anchor='n')
+frame.grid(row=0, column=0)
+
 
 entry = tk.Entry(frame, font=40)
-entry.place(relwidth=0.65, relheight=1)
+#entry.place(relwidth=0.65, relheight=1)
+entry.grid(row=0, column=0)
 
 button = tk.Button(frame, text="Get Weather", font=40, command=lambda: get_weather(entry.get()))
-button.place(relx=0.7, relheight=1, relwidth=0.3)
+#button.place(relx=0.7, relheight=1, relwidth=0.3)
+button.grid(row=1, column=1)
+
 
 lower_frame = tk.Frame(root, bg='#80c1ff', bd=10)
-lower_frame.place(relx=0.5, rely=0.25, relwidth=0.75, relheight=0.6, anchor='n')
+#lower_frame.place(relx=0.5, rely=0.25, relwidth=0.75, relheight=0.6, anchor='n')
+lower_frame.grid(row=1, column=0)
 
-label = tk.Label(lower_frame)
-label.place(relwidth=1, relheight=1)
+
+label = tk.Label(frame)
+#label.place(relwidth=1, relheight=1)
+lower_frame.grid(row=3, column=0)
+
+
+lower_frame = tk.Frame(root, bg='#80c1ff', bd=10)
+#lower_frame.place(relx=0.5, rely=0.25, relwidth=0.75, relheight=0.6, anchor='n')
+lower_frame.grid(row=1, column=0)
 
 root.mainloop()
