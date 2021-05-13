bcb58a0cdc164537324f19182bdd5f97796e19f9
Nate Herbert <nateherbert@gmail.com>
2021-05-13 12:30:00 -0500

    slight cleanup

--- avalon.py
+++ avalon.py
@@ -45,33 +45,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-
-# check user input yes/no
-# check received stuff from server yes/no
-
-
-# polling driven
-# I'm doing my own thing, OS, you keep track of stuff, every once in a while I'll ask you, 'hey, did a thing happen?'
-
-
-# nonblocking asencrunus networking (IO)
-# I'm expecting I might receive data from the server. If I do, please put it in this variable.
-# then go into UI and check if there is user input or not
-# have I received infromation from the server?
-# if the answer is no, then loop otherwise update.
-# sleep 10 miliseconds
-
-
-# interupt driven
-# wake me up when an event happens, otherwise I'm going to sleep.
-
-
-# headers:
-# the first int is how long the message is
-# example, the first message is a string with a new line character at the end. It's a # with how many characters are after this
-# the second message is the actual message
-
-
-# 'stop code' is the second way to handle this
-# keep fetching until I get the keyboard ex: "!stop"
